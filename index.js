d795728503ab75a5cc6f53cdd2be9dc2f473f625
ggniutti <162546109+ggniutti@users.noreply.github.com>
2024-03-07 09:57:50 +0330

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching 💠Special Friends💠"];
 
 
 let currentIndex = 0;
